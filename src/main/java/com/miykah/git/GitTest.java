dc35d41777fcaef05bf984259f4572eecb51cf35
Miykah <miykah@163.com>
2021-11-23 14:01:59 +0800

    master commit

--- src/main/java/com/miykah/git/GitTest.java
+++ src/main/java/com/miykah/git/GitTest.java
@@ -12,7 +12,7 @@ public class GitTest {
         System.out.println("hello git again!");
         System.out.println("hello git again 2!");
         System.out.println("hello git again 3!");
-        System.out.println("hello git hot-fix");
+        System.out.println("hello git master");
 
     }
 
