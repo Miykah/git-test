8aa1b01cd1a26e8a10c66d696e550d6ad047edaa
Miykah <miykah@163.com>
2021-11-23 13:45:31 +0800

    first commit

---
+++ src/main/java/com/miykah/git/GitTest.java
@@ -0,0 +1,16 @@
+package com.miykah.git;
+
+/**
+ * @author miykah
+ * @create 2021-11-23 13:34
+ */
+public class GitTest {
+
+    public static void main(String[] args) {
+
+        System.out.println("hello git!");
+        System.out.println("hello git again!");
+
+    }
+
+}
